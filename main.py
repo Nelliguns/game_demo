08cc77beb7b1ee3b6ecd1feff26858d239156575
Liam Nelligan <liamnelligan000@gmail.com>
2024-09-29 11:40:45 +0200

    window

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+# from pygame.locals import * 
+
+pygame.init()
+clock = pygame.Clock
+pygame.display.set_caption("My game!")
+
+WINDOW_SIZE = (400, 400)
+
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+
+
+    pygame.display.update()
+    clock.tick(60)
